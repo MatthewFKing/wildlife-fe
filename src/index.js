4f5f3b7c8dde89c6421b2816a2023bb11b247de9
matthewfking <kingmathu@gmail.com>
2017-11-28 21:50:02 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' component={App}></Route>
+      </Switch>
+    </BrowserRouter>
+  </Provider>
+  , document.getElementById('root'));
